42ccb933be0c616229007eb0f1ac0448da6fa1fb
prabinsharmaa <prabinent7@gmail.com>
2021-12-26 17:58:27 -0500

    Homework part 2

---
+++ CS680 homework 10/src/edu/umb/cs680/hw10/apfs/APFS.java
@@ -0,0 +1,26 @@
+package edu.umb.cs680.hw10.apfs;
+
+import java.time.LocalDateTime;
+import edu.umb.cs680.hw10.fs.FSElement;
+import edu.umb.cs680.hw10.fs.FileSystem;
+
+public class APFS extends FileSystem {
+
+	private static LocalDateTime localTime = LocalDateTime.now();
+	private static APFS instance = null;
+
+	private APFS() {
+	}
+
+	public static APFS getAPFSFileSystem() {
+		if (instance == null)
+			instance = new APFS();
+		return instance;
+	}
+
+	@Override
+	protected FSElement createDefaultRoot() {
+		return new ApfsDirectory(null, "root", 0, localTime, "Prabin", localTime);
+	}
+
+}
---
+++ CS680 homework 10/src/edu/umb/cs680/hw10/apfs/ApfsVisitor.java
@@ -0,0 +1,11 @@
+package edu.umb.cs680.hw10.apfs;
+
+public interface ApfsVisitor {
+
+	public void visit(ApfsLink link);
+
+	public void visit(ApfsDirectory dir);
+
+	public void visit(ApfsFile file);
+
+}
